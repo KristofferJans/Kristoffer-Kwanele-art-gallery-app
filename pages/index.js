308ea6a768e7d96f36c9c575502950696dd9e705
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-10 15:07:42 +0200

    implemented data fetch via useSWR hook

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,30 @@
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const fetcher = async (url) => {
+    const response = await fetch(url);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!response.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      // Attach extra info to the error object.
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return response.json();
+  };
+  const { data, mutate, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log("data", data);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
