a4facfcccb664d0579f17037da58134f0e967a25
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-11 15:43:01 +0200

    implemented redirecting to the details page when you click on art piece

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,23 +1,31 @@
 import Image from "next/image";
 import List from "../List";
+import Link from "next/link";
 
 export default function ArtPieces({ pieces }) {
   return (
     <ul>
       {pieces.map((piece) => (
         <li key={piece.slug}>
-          <div>
-            <h2>{piece.name}</h2>
-            <Image
-              src={piece.imageSource}
-              alt={piece.name}
-              height={144}
-              width={144}
-            />
-            <p>Artist: {piece.artist}</p>
-          </div>
+          <Link legacyBehavior href={`/art-pieces/${piece.slug}`}>
+            <a style={{ textDecoration: "none", color: "inherit" }}>
+              <h2>{piece.name}</h2>
+              <Image
+                src={piece.imageSource}
+                alt={piece.name}
+                height={144}
+                width={144}
+              />
+              <p>Artist: {piece.artist}</p>
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
   );
 }
+
+// Next.js Link component for navigating to the details page of the art piece
+// The legacyBehavior prop is used to maintain the old behavior of Link wrapping an <a> tag
+// Without the a tag with the inline styling and the legacy behavior prop, everything is displayed as a link -
+// that's why we added this here, so that you can click anywhere and be redirected to the details page but the content is not displayed as a link, but normally
